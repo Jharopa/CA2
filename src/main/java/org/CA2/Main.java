57e2ea3ff42daec33d193576af32e74e94ee1beb
lhartnet <laura.hartnett@outlook.com>
2023-11-15 20:36:50 +0000

    Laura's first test commit

--- src/main/java/org/CA2/Main.java
+++ src/main/java/org/CA2/Main.java
@@ -3,5 +3,6 @@ package org.CA2;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Laura says hi");
     }
 }
