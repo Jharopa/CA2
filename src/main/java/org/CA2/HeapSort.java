e5664cac8f9a8f27ad23155e70a92ac998d8c982
Alex Burns <jharopa@gmail.com>
2023-11-19 16:17:02 +0000

    Generic heapsort

---
+++ src/main/java/org/CA2/HeapSort.java
@@ -0,0 +1,43 @@
+package org.CA2;
+
+// Usage: HeapSort.<TYPE>sort(array); where type is the type of array being sorted
+// This will work for any type that implements the comparable interface
+
+public class HeapSort {
+    public static <T extends Comparable<T>> void sort(T[] arr) {
+        int n = arr.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        for (int i = n - 1; i > 0; i--) {
+            T temp =  arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, i, 0);
+        }
+    }
+
+    private static <T extends Comparable<T>> void heapify(T[] arr, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left].compareTo(arr[largest]) > 0) {
+            largest = left;
+        }
+
+        if (right < n && arr[right].compareTo(arr[largest]) > 0) {
+            largest = right;
+        }
+        if (largest != i) {
+            T swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            heapify(arr, n, largest);
+        }
+    }
+}
